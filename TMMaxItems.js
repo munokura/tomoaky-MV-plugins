c4ad8625b9981e96f766679e0246e8139e07116a
munokura <munokura@users.noreply.github.com>
2020-10-13 08:09:53 +0900

    MZで動作確認

--- TMMaxItems.js
+++ TMMaxItems.js
@@ -10,11 +10,13 @@
 //=============================================================================
 
 /*:
+ * @target MZ MV
+ * @url https://raw.githubusercontent.com/munokura/tomoaky-MV-plugins/master/TMMaxItems.js
+ * @author tomoaky (https://twitter.com/tomoaky/)
  * @plugindesc アイテムの所持上限数を個別に設定します。
  *
- * @author tomoaky (http://hikimoki.sakura.ne.jp/)
- *
  * @param numberMaxDigits
+ * @text 最大所持上限のケタ数
  * @type number
  * @min 1
  * @desc 最大所持上限のケタ数
@@ -22,12 +24,14 @@
  * @default 4
  * 
  * @param numberFontSize
+ * @text 個数表示のフォントサイズ
  * @type number
  * @desc 個数表示のフォントサイズ
  * 初期値: 24
  * @default 24
  * 
  * @param numberSeparator
+ * @text 個数の前に表示する文字列
  * @desc 個数の前に表示する文字列
  * @default ×
  * 
@@ -40,11 +44,6 @@
  *   タグがないアイテムの所持上限数は 99 のままです。
  * 
  *   プラグインコマンドはありません。
- *
- *   このプラグインは RPGツクールMV Version 1.5.1 で動作確認をしています。
- * 
- *   このプラグインはMITライセンスのもとに配布しています、商用利用、
- *   改造、再配布など、自由にお使いいただけます。
  * 
  * 
  * 注意:
@@ -53,12 +52,20 @@
  *   Window_ShopNumber の maxDigits を上書きしています。
  *   プラグイン管理でこのプラグインをなるべく上の方に配置すると
  *   他のプラグインとの競合を回避できるかもしれません。
- */
+ *
+ * 
+ * 利用規約:
+ *   MITライセンスです。
+ *   https://ja.osdn.net/projects/opensource/wiki/licenses%2FMIT_license
+ *   作者に無断で改変、再配布が可能で、
+ *   利用形態（商用、18禁利用等）についても制限はありません。
+*/
 
 var Imported = Imported || {};
 Imported.TMMaxItems = true;
 
-(function() {
+(function () {
+  'use strict';
 
   var parameters = PluginManager.parameters('TMMaxItems');
   var numberMaxDigits = +(parameters['numberMaxDigits'] || 4);
@@ -70,7 +77,7 @@ Imported.TMMaxItems = true;
   //
 
   var _Game_Party_maxItems = Game_Party.prototype.maxItems;
-  Game_Party.prototype.maxItems = function(item) {
+  Game_Party.prototype.maxItems = function (item) {
     if (item.meta.maxItems) return Math.max(+item.meta.maxItems, 1);
     return _Game_Party_maxItems.call(this, item);
   };
@@ -79,7 +86,7 @@ Imported.TMMaxItems = true;
   // Window_ItemList
   //
 
-  Window_ItemList.prototype.numberWidth = function() {
+  Window_ItemList.prototype.numberWidth = function () {
     var fontSize = this.contents.fontSize;
     var result;
     this.contents.fontSize = numberFontSize;
@@ -88,7 +95,7 @@ Imported.TMMaxItems = true;
     return result;
   };
 
-  Window_ItemList.prototype.drawItemNumber = function(item, x, y, width) {
+  Window_ItemList.prototype.drawItemNumber = function (item, x, y, width) {
     var fontSize = this.contents.fontSize;
     var numberWidth;
     if (this.needsNumber()) {
@@ -104,7 +111,7 @@ Imported.TMMaxItems = true;
   // Window_ShopNumber
   //
 
-  Window_ShopNumber.prototype.maxDigits = function() {
+  Window_ShopNumber.prototype.maxDigits = function () {
     return numberMaxDigits;
   };
 
