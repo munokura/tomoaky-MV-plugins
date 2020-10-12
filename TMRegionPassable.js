64f82b013680eb0f9a3fa3fbce77a63c6988c9f5
munokura <36838778+munokura@users.noreply.github.com>
2020-10-12 15:41:40 +0900

    MZで動作確認

--- TMRegionPassable.js
+++ TMRegionPassable.js
@@ -10,22 +10,32 @@
 //=============================================================================
 
 /*:
+ * @target MZ MV
+ * @url https://raw.githubusercontent.com/munokura/tomoaky-MV-plugins/master/TMRegionPassable.js
+ * @author tomoaky (https://twitter.com/tomoaky/)
  * @plugindesc その場所が通行可能かどうかをリージョンで設定できるようになります。
  *
- * @author tomoaky (http://hikimoki.sakura.ne.jp/)
- *
  * @param passableRegions
- * @desc タイルに関係なく通行を可能にするリージョン番号
+ * @text 通行可能リージョン
+ * @type string
+ * @desc タイルに関係なく通行を可能にするリージョン番号。
+ * 半角スペース区切りで複数設定
  * 初期値: 251
  * @default 251
  *
  * @param dontPassRegions
- * @desc タイルに関係なく通行を不可にするリージョン番号
+ * @text 通行不可リージョン
+ * @type string
+ * @desc タイルに関係なく通行を不可にするリージョン番号。
+ * 半角スペース区切りで複数設定
  * 初期値: 252 253
  * @default 252 253
  *
  * @param counterRegions
- * @desc カウンター属性をもたせるリージョン番号
+ * @text カウンター属性リージョン
+ * @type string
+ * @desc カウンター属性をもたせるリージョン番号。
+ * 半角スペース区切りで複数設定
  * 初期値: 253
  * @default 253
  *
@@ -44,39 +54,42 @@
  * 
  *   プラグインコマンドはありません。
  *
- *   このプラグインは RPGツクールMV Version 1.5.1 で動作確認をしています。
  * 
- *   このプラグインはMITライセンスのもとに配布しています、商用利用、
- *   改造、再配布など、自由にお使いいただけます。
+ * 利用規約:
+ *   MITライセンスです。
+ *   https://ja.osdn.net/projects/opensource/wiki/licenses%2FMIT_license
+ *   作者に無断で改変、再配布が可能で、
+ *   利用形態（商用、18禁利用等）についても制限はありません。
  */
 
 var Imported = Imported || {};
 Imported.TMRegionPassable = true;
 
 (function() {
+    'use strict';
 
-  var parameters = PluginManager.parameters('TMRegionPassable');
-  var passableRegions = (parameters['passableRegions'] || '251').split(' ').map(Number);
-  var dontPassRegions = (parameters['dontPassRegions'] || '252 253').split(' ').map(Number);
-  var counterRegions = (parameters['counterRegions'] || '253').split(' ').map(Number);
+    var parameters = PluginManager.parameters('TMRegionPassable');
+    var passableRegions = (parameters['passableRegions'] || '251').split(' ').map(Number);
+    var dontPassRegions = (parameters['dontPassRegions'] || '252 253').split(' ').map(Number);
+    var counterRegions = (parameters['counterRegions'] || '253').split(' ').map(Number);
 
-  //-----------------------------------------------------------------------------
-  // Game_Map
-  //
+    //-----------------------------------------------------------------------------
+    // Game_Map
+    //
 
-  var _Game_Map_checkPassage = Game_Map.prototype.checkPassage;
-  Game_Map.prototype.checkPassage = function(x, y, bit) {
-    var regionId = this.regionId(x, y);
-    if (passableRegions.indexOf(regionId) >= 0) return true;
-    if (dontPassRegions.indexOf(regionId) >= 0) return false;
-    return _Game_Map_checkPassage.call(this, x, y, bit);
-  };
+    var _Game_Map_checkPassage = Game_Map.prototype.checkPassage;
+    Game_Map.prototype.checkPassage = function(x, y, bit) {
+        var regionId = this.regionId(x, y);
+        if (passableRegions.indexOf(regionId) >= 0) return true;
+        if (dontPassRegions.indexOf(regionId) >= 0) return false;
+        return _Game_Map_checkPassage.call(this, x, y, bit);
+    };
 
-  var _Game_Map_isCounter = Game_Map.prototype.isCounter;
-  Game_Map.prototype.isCounter = function(x, y) {
-    var regionId = this.regionId(x, y);
-    if (counterRegions.indexOf(regionId) >= 0) return true;
-    return _Game_Map_isCounter.call(this, x, y);
-  };
+    var _Game_Map_isCounter = Game_Map.prototype.isCounter;
+    Game_Map.prototype.isCounter = function(x, y) {
+        var regionId = this.regionId(x, y);
+        if (counterRegions.indexOf(regionId) >= 0) return true;
+        return _Game_Map_isCounter.call(this, x, y);
+    };
 
 })();
