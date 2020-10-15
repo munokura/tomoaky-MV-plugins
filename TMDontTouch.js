57118237928d442ab8701b96a83932e113874041
munokura <munokura@users.noreply.github.com>
2020-10-15 19:47:29 +0900

    MZで動作確認

--- TMDontTouch.js
+++ TMDontTouch.js
@@ -6,11 +6,15 @@
 //=============================================================================
 
 /*:
+ * @target MZ MV
+ * @url https://raw.githubusercontent.com/munokura/tomoaky-MV-plugins/master/TMDontTou.js
+ * @author tomoaky (https://twitter.com/tomoaky/)
  * @plugindesc 指定したリージョンをタップ（クリック）できなくします。
- * 
- * @author tomoaky (http://hikimoki.sakura.ne.jp/)
  *
  * @param Dont Touch Region
+ * @text タップ不可リージョン番号
+ * @type number
+ * @max 255
  * @desc タップ（クリック）できないリージョン番号。
  * 初期値: 1
  * @default 1
@@ -22,6 +26,13 @@
  * 逆にボタンピクチャが下にあるとボタンピクチャが反応します。
  *
  * プラグインコマンドはありません。
+ * 
+ * 
+ * 利用規約:
+ *   MITライセンスです。
+ *   https://ja.osdn.net/projects/opensource/wiki/licenses%2FMIT_license
+ *   作者に無断で改変、再配布が可能で、
+ *   利用形態（商用、18禁利用等）についても制限はありません。
  */
 
 var Imported = Imported || {};
@@ -43,4 +54,3 @@ Scene_Map.prototype.processMapTouch = function() {
     Tomoaky.DT.Scene_Map_processMapTouch.call(this);
   }
 };
-
