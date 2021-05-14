b6acafb8d435a72182c8a11fb0c5a26732408217
munokura <munokura@users.noreply.github.com>
2021-05-15 06:01:48 +0900

    プラグインパラメーターの調整

--- TMBattlerEx.js
+++ TMBattlerEx.js
@@ -12,32 +12,43 @@
 /*:
  * @plugindesc エネミーに遠近感や息づかいの表現を追加します。
  *
- * @author tomoaky (http://hikimoki.sakura.ne.jp/)
+ * @author tomoaky
  *
  * @param baseY
+ * @text 拡大率が等倍になるＹ座標
  * @desc 拡大率が等倍になるＹ座標。
  * 初期値: 400
  * @default 400
- *
+ * 
  * @param breathH
+ * @text 息づかいの大きさ
  * @desc 息づかいの大きさ。
  * 初期値: 0.05
  * @default 0.05
- *
+ * 
  * @param mirrorRate
+ * @text 左右反転の確率
  * @desc 左右反転の確率。
  * 初期値: 0.4（ 0 ～ 1 ）
  * @default 0.4
- *
+ * 
  * @param breathStop
+ * @text 行動不能時呼吸
  * @desc 行動不能時に息づかいを止める。
- * 初期値: 1（ 0 で無効 / 1 で有効 ）
- * @default 1
- *
+ * 初期値: true
+ * @type boolean
+ * @on 有効
+ * @off 無効
+ * @default true
+ * 
  * @param shakeEffect
+ * @text 点滅エフェクト差替
  * @desc 点滅エフェクトを揺れエフェクトに差し替える。
- * 初期値: 1（ 0 で無効 / 1 で有効 ）
- * @default 1
+ * 初期値: true
+ * @type boolean
+ * @on 有効
+ * @off 無効
+ * @default true
  *
  * @help
  * 使い方:
@@ -53,8 +64,6 @@
  *
  *   プラグインコマンドはありません。
  *
- *   このプラグインは RPGツクールMV Version 1.3.0 で動作確認をしています。
- *
  *
  * メモ欄（敵キャラ）タグ:
  *
@@ -67,6 +76,14 @@
  *
  *   <noMirror>
  *     左右反転を禁止します。
+ * 
+ * 2021/5/15 v.2.0.1 プラグインパラメーターの調整
+ *
+ * 利用規約:
+ *   MITライセンスです。
+ *   https://licenses.opensource.jp/MIT/MIT.html
+ *   作者に無断で改変、再配布が可能で、
+ *   利用形態（商用、18禁利用等）についても制限はありません。
  */
 
 var Imported = Imported || {};
@@ -78,36 +95,36 @@ TMPlugin.BattlerEx.Parameters = PluginManager.parameters('TMBattlerEx');
 TMPlugin.BattlerEx.BaseY = +(TMPlugin.BattlerEx.Parameters['baseY'] || 400);
 TMPlugin.BattlerEx.BreathH = +(TMPlugin.BattlerEx.Parameters['breathH'] || 0.05);
 TMPlugin.BattlerEx.MirrorRate = +(TMPlugin.BattlerEx.Parameters['mirrorRate'] || 0.4);
-TMPlugin.BattlerEx.BreathStop = TMPlugin.BattlerEx.Parameters['breathStop'] === '1';
-TMPlugin.BattlerEx.ShakeEffect = TMPlugin.BattlerEx.Parameters['shakeEffect'] === '1';
+TMPlugin.BattlerEx.BreathStop = TMPlugin.BattlerEx.Parameters['breathStop'] === 'true';
+TMPlugin.BattlerEx.ShakeEffect = TMPlugin.BattlerEx.Parameters['shakeEffect'] === 'true';
 
-(function() {
+(function () {
 
   //-----------------------------------------------------------------------------
   // Sprite_Enemy
   //
-  
+
   var _Sprite_Enemy_initialize = Sprite_Enemy.prototype.initialize;
-  Sprite_Enemy.prototype.initialize = function(battler) {
+  Sprite_Enemy.prototype.initialize = function (battler) {
     _Sprite_Enemy_initialize.call(this, battler);
     var r = +(battler.enemy().meta.scale || this.y / (TMPlugin.BattlerEx.BaseY * 2) + 0.5);
     this._baseScale = new Point(r, r);
     if (!$gameSystem.isSideView() && Math.random() < TMPlugin.BattlerEx.MirrorRate &&
-        !battler.enemy().meta.noMirror) {
+      !battler.enemy().meta.noMirror) {
       this._baseScale.x = 0 - r;
       this._stateIconSprite.scale.x = -1;
     }
     this._breathMax = Math.randomInt(90) + 90;
     this._breathCount = Math.randomInt(this._breathMax);
   };
-  
+
   var _Sprite_Enemy_update = Sprite_Enemy.prototype.update;
-  Sprite_Enemy.prototype.update = function() {
+  Sprite_Enemy.prototype.update = function () {
     _Sprite_Enemy_update.call(this);
     this.updateScaleEx();
   };
 
-  Sprite_Enemy.prototype.updateScaleEx = function() {
+  Sprite_Enemy.prototype.updateScaleEx = function () {
     if (TMPlugin.BattlerEx.BreathStop && !this._enemy.canMove()) return;
     this._breathCount++;
     if (this._breathCount >= this._breathMax) {
@@ -122,7 +139,7 @@ TMPlugin.BattlerEx.ShakeEffect = TMPlugin.BattlerEx.Parameters['shakeEffect'] ==
   };
 
   var _Sprite_Enemy_updateBlink = Sprite_Enemy.prototype.updateBlink;
-  Sprite_Enemy.prototype.updateBlink = function() {
+  Sprite_Enemy.prototype.updateBlink = function () {
     if (TMPlugin.BattlerEx.ShakeEffect) {
       var ed = this._effectDuration;
       this.rotation = ed % 4 < 2 ? ed / 200 : 0 - ed / 200;
